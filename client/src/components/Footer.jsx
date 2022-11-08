2c0ad441cf8e3f4f2d4c11a04cb02fea73aa72c1
Ehtesham Faraz <efaraz27@gmail.com>
2022-11-08 17:47:34 +0530

    style changes

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import logo from "../../images/logo.png";
 
 const Footer = () => (
   <div className="w-full flex md:justify-center justify-between items-center flex-col p-4 gradient-bg-footer">
-    <div className="w-full flex sm:flex-row flex-col justify-between items-center my-4">
+    {/* <div className="w-full flex sm:flex-row flex-col justify-between items-center my-4">
       <div className="flex flex-[0.5] justify-center items-center">
         <img src={logo} alt="logo" className="w-32" />
       </div>
@@ -12,7 +12,7 @@ const Footer = () => (
         <p className="text-white text-base text-center mx-2 cursor-pointer">Market</p>
         <p className="text-white text-base text-center mx-2 cursor-pointer">Exchange</p>
       </div>
-    </div>
+    </div> */}
 
     <div className="flex justify-center items-center flex-col mt-5">
       <p className="text-white text-sm text-center">Your Account is safe here</p>
